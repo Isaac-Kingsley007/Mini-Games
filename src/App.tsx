87703e7d6bdbdc5897e770c1d81795c339b2bb63
Isaac-Kingsley007 <160049323+Isaac-Kingsley007@users.noreply.github.com>
2025-06-06 19:58:20 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return(
+    <div className="flex flex-col items-center">
+      <div className="text-red-700 bg-pink-500 inline-block p-2 rounded-lg"> Hello World</div>
+    </div>
+  )
+}
+
+export default App
